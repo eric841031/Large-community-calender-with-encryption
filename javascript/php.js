2f8a0030288e4f22c760d12dde1d8b128baea292
ren1244 <n1244506804@hotmail.com>
2015-10-18 23:50:39 +0800

    增加註解

--- javascript/php.js
+++ javascript/php.js
@@ -1,3 +1,17 @@
+/*
+函數:
+	call_php(url,params,cbk_func,ststus_div)
+參數:
+	url:希望連結的伺服器。資料型態:字串
+	params:傳送給伺服器的參數。資料型態:字串
+	cbk_func:當伺服器回傳訊息時，要執行的內容。資料型態:字串
+	status_div(可選):顯示連線狀態的div物件。資料型態:Element
+取得伺服器回傳:
+	用cbk_func="ret_val=http.responseText;"可以將回傳值存在變數ret_val中
+範例:
+	call_php("http://Demo/sever.php","user=Lucky","ans=http.responseText;f1()");
+	將字串"user=Lucky"送給伺服器，當伺服器回傳後時，將資料存在變數ans中，並呼叫函數f1。不顯示連線狀態。
+*/
 function call_php(url,params,cbk_func,ststus_div)
 {
 	var http = new XMLHttpRequest();
